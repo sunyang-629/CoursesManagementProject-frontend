4521a8c00100510a07ea0fb82c434942ae8809fe
sunyang9876 <sunyang9876@hotmail.com>
2019-12-26 11:51:44 +1000

    add course.js lecturers.js and students.js

---
+++ course/Courses.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Course = () => {
+    return <div>This is courses page</div>
+}
+
+export default Course;
---
+++ lecturer/Lecturers.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Lecturer = () => {
+    return <div>this is Lecturer page</div>
+}
+
+export default Lecturer;
